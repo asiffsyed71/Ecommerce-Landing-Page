c6b724dd200baa678fa887a3dc83f1b9d6bda1b5
Asif Iqbal <asifsyed71@hotmail.com>
2023-09-19 03:31:24 +0530

    code for Landing page

---
+++ src/components/MobileMenu.jsx
@@ -0,0 +1,53 @@
+import React, { useLayoutEffect } from 'react'
+import { navLinks } from '../constants';
+
+const MobileMenu = ({mobileMennuActive}) => {
+
+  useLayoutEffect(() => {
+    let windowWidth = window.innerWidth;
+    console.log(windowWidth)
+},[window.innerWidth])
+  return (
+    <section
+        id="mobile-menu"
+        className={`absolute top-[6rem] sm:right-[4rem] right-[2rem]  w-1/3 bg-stone-100 p-8 text-5xl origin-top animate-open-menu md:${mobileMennuActive ? 'flex' : 'hidden'}`}
+      >
+        {/* <nav
+          className="flex flex-col min-h-screen items-center py-8 text-4xl"
+          aria-label="mobile"
+        >
+          <a href="#hero" className="w-full text-center py-6 hover:opacity-90"
+            >Home</a
+          >
+          <a href="#rockets" className="w-full text-center py-6 hover:opacity-90"
+            >Rockets</a
+          >
+          <a
+            href="#testimonials"
+            className="w-full text-center py-6 hover:opacity-90"
+            >Testimonials</a
+          >
+          <a href="#contact" className="w-full text-center py-6 hover:opacity-90"
+            >Contact Us</a
+          >
+          <a href="#footer" className="w-full text-center py-6 hover:opacity-90"
+            >Legal</a
+          >
+        </nav> */}
+         <ul className="flex-1 flex flex-col justify-center items-center gap-12">
+          {navLinks.map((navItem) => (
+            <li key={navItem.label}>
+              <a
+                href={navItem.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {navItem.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </section>
+  )
+}
+
+export default MobileMenu;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,54 @@
+import { useEffect, useLayoutEffect, useState } from "react";
+import { hamburger } from "../assets/icons";
+import { headerLogo } from "../assets/images";
+import { navLinks } from "../constants";
+import MobileMenu from "./MobileMenu";
+import useWindowSize from "../hooks/useWindowSize";
+
+const Nav = () => {
+    const [mobileMennuActive, setMobileMenuActive] = useState(false);
+    const [toggleHamburger, setToggleHamburger] = useState(false);
+
+    const [size] = useWindowSize();
+
+    const handleClick = () => {
+        setToggleHamburger((prev) => !prev )
+        setMobileMenuActive((prev) => !prev )
+
+    }
+  return (
+    <header className="padding-x py-8  z-50 w-full sticky top-0 bg-white">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="logo" width={130} height={28} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((navItem) => (
+            <li key={navItem.label}>
+              <a
+                href={navItem.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {navItem.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+
+        {/* <div className="hidden max-lg:block">
+            <img src={hamburger} alt="hamburger icon" width={25} height={25}/>
+        </div> */}
+        <button
+          id="hamburger-button"
+          className={`text-3xl hidden max-lg:block cursor-pointer relative w-8 h-8 ${toggleHamburger ? 'toggle-btn' : '' }`}
+          onClick={handleClick}
+        >
+          <div className="bg-slate-gray w-8 h-1 rounded absolute top-4 -mt-0.5 transition-all duration-400 before:content-[''] before:bg-slate-gray before:h-1 before:w-8 before:absolute before:rounded before:transition-all before:duration-500 before:-translate-x-4 before:-translate-y-3 after:content-[''] after:bg-slate-gray after:h-1 after:w-8 after:absolute after:rounded after:transition-all after:duration-500 after:-translate-x-4 after:translate-y-3"></div>
+        </button>
+      </nav>
+      <MobileMenu mobileMennuActive={size < 1024 && mobileMennuActive}/>
+    </header>
+  );
+};
+
+export default Nav;
